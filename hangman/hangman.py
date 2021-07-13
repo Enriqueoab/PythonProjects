0173440eafa7f11b8a63e4336f2abd9ca646df0d
Enrique Barca <barcaenrique7@gmail.com>
2021-07-13 11:33:09 +0100

    Reg. expression to valid the word and path to the words resoucefixed

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,21 +1,26 @@
 import random
 import string
-from words import words
+import re
 #That way we can get the resources which we create on the words document (The words array)
+from words import words
 
 def getWord(words):
-#Ramdomly we get a word from the list
-    choosedWord=random.choice(words)
-    
-    return choosedWord.upper()
+    #Ramdomly we get a word from the list
+    choseWord=random.choice(words)
+    #This loop make sure that the word hasn't any other caracter but letters
+    while re.search('^[a-zA-Z]+$',choseWord) is None:
+        choseWord=random.choice(words)
+
+    return choseWord.upper()
 
 def hangman():
     
-    choosedWord=getWord(words)
+    choseWord=getWord(words)
 
     #Using set we store just the differents letters of the word
-    wordLetters=set(choosedWord)
+    wordLetters=set(choseWord)
     usedLetters=set()
+    alphabet = set(string.ascii_uppercase)
     #userLetter=input("Guess a letter").upper
     endGame=False
     
@@ -31,7 +36,7 @@ def hangman():
                     endGame=True
                 if userLetter not in wordLetters:
                     usedLetters.add(userLetter)
-                elif userLetter == choosedWord:
+                elif userLetter == choseWord:
                     print("Congratulations you guess the word!!")
                     endGame=True
         else:
