b76d8d9dfe2544d63442e735f308eb37b90d0d04
Enrique Barca <barcaenrique7@gmail.com>
2021-07-25 10:48:23 +0100

    Basic html template to show data, and update view.index function to send the context to the template

--- Python tutorial/ecommerce/products/views.py
+++ Python tutorial/ecommerce/products/views.py
@@ -1,19 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # We have to map localhost.../product to index
 # We are going to set it in the 
 # products/urls.py file
 
-
 # Web page views by URL 
 def index(request):
-    return HttpResponse('Hello word')
+    products = Product.objects.all()    # {'products': products} -> context to use in the template
+    return render(request, 'index.html',{'products': products}) 
 
 #That would be our localhost.../product/new
 def product_new(request):
     return HttpResponse('Hello new word')
 
 def product_offer(request):
-    return HttpResponse('Offer view web')
+    return HttpResponse('Offer view webviews')
     
